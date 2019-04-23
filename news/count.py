d4e414201079245e2ff27182381996a41bc3e25e
phe <369526049@qq.com>
2019-04-23 14:53:19 +0800

    update content

--- news/count.py
+++
@@ -1,5 +0,0 @@
-with open('news/news.txt', 'r') as f:
-    source = f.read().split('\n')
-    print(source)
-from collections import Counter
-print(Counter(source))
--- news/news/pipelines.py
+++ news/news/pipelines.py
@@ -4,16 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import json
+
+# If you just want text content, use command to get it.
 
 
 class NewsPipeline(object):
-    def __init__(self):
-        self.file = open('news/news_content.txt', mode='w', encoding='gbk')
+    # def __init__(self):
+    #     self.file = open('news/news_content.txt', mode='w', encoding='gbk')
 
     def process_item(self, item, spider):
-        self.file.write(item['content'] + '\n' + '*' * 100 + '\n')
+        # self.file.write(item['content'] + '\n' + '*' * 100 + '\n')
         return item
 
-    def close_spider(self, spider):
-        self.file.close()
+    # def close_spider(self, spider):
+    #     self.file.close()
